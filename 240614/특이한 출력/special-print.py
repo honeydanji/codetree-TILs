396c8b4acd452baa4c12a5a36659bf4ae62e5a15
HA SEONG JIN <129818881+honeydanji@users.noreply.github.com>
2024-06-14 16:50:51 +0900

    Title: 특이한 출력, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240614/특이한 출력/special-print.py
+++ 240614/특이한 출력/special-print.py
@@ -3,5 +3,5 @@ n = int(input())
 for i in range(1, n+1):
     for j in range(1, n+1):
         if (i+j) % 4 == 0:
-            print("\n")
+            print()
         print(f"({i}, {j})", end=" ")
