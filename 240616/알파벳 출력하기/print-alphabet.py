623348ba59803f2fbad7ab1e543af960ce6af321
HA SEONG JIN <129818881+honeydanji@users.noreply.github.com>
2024-06-16 22:25:18 +0900

    Title: 알파벳 출력하기, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240616/알파벳 출력하기/print-alphabet.py
+++ 240616/알파벳 출력하기/print-alphabet.py
@@ -5,7 +5,7 @@ a = 65
 for i in range(1, n+1):
     for j in range(i):
         print(chr(a), end="")
-        a+=1
         if chr(a)=="Z":
-            a = 65
+            a = 64
+        a+=1
     print()
