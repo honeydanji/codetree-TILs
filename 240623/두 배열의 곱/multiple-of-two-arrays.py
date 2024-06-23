274a1c8477493730c7a9f57421acb289677ddeda
HA SEONG JIN <129818881+honeydanji@users.noreply.github.com>
2024-06-23 22:36:53 +0900

    Title: 두 배열의 곱, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240623/두 배열의 곱/multiple-of-two-arrays.py
@@ -0,0 +1,17 @@
+arr = []
+brr = []
+
+
+for _ in range(3):
+    arr.append(list(map(int, input().split())))
+
+input()
+
+for _ in range(3):
+    brr.append(list(map(int, input().split())))
+
+
+for i in range(3):
+    for j in range(3):
+        print(arr[i][j] * brr[i][j], end=" ")
+    print()
