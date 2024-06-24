772c2b1b60015225a90423e2b2dc118f14e9424a
HA SEONG JIN <129818881+honeydanji@users.noreply.github.com>
2024-06-24 20:25:26 +0900

    Title: 대각선으로 숫자 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240624/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -0,0 +1,35 @@
+a, b = map(int, input().split())
+
+arr = [[0 for _ in range(b)] for _ in range(a)]
+
+num = 1
+n = 0
+m = 0
+k = 0
+l = 0
+c = 0
+
+while True:
+    if num > a*b:
+        break
+    n = l
+    m = k
+    while True:
+        arr[n][m] = num
+        num += 1
+        if m == c:
+            if k < b-1: # b-1 2 ////  k 1 2
+                k += 1
+            else:
+                l += 1
+                c += 1
+            break
+        else:
+            n += 1
+            m -= 1
+
+    
+for i in range(a):
+    for j in range(b):
+        print(arr[i][j], end=" ")
+    print()
