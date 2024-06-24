dddb43e6d4503617ae7d364c29f48399ac9cb723
HA SEONG JIN <129818881+honeydanji@users.noreply.github.com>
2024-06-24 21:07:06 +0900

    Title: 대각선으로 숫자 채우기, Time: 142ms, Memory: 28MB, Status: Passed - CodeTree

--- 240624/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 240624/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -3,32 +3,21 @@ a, b = tuple(map(int, input().split()))
 arr = [[0 for _ in range(b)] for _ in range(a)]
 
 num = 1
-n = 0
-m = 0
-k = 0
-l = 0
-c = 0
 
-while True:
-    if num > a*b:
-        break
-    n = l
-    m = k
-    while True:
-        arr[n][m] = num
-        num += 1
-        if m == c or n == a-1:
-            if k < b-1:
-                k += 1
-            else:
-                l += 1
-                c += 1
-            break
-        else:
-            n += 1
-            m -= 1
-
+for diag in range(a + b - 1):
+    if diag < b:
+        x = 0
+        y = diag
+    else:
+        x = diag - b + 1
+        y = b - 1
     
+    while x < a and y >= 0:
+        arr[x][y] = num
+        num += 1
+        x += 1
+        y -= 1
+
 for i in range(a):
     for j in range(b):
         print(arr[i][j], end=" ")
