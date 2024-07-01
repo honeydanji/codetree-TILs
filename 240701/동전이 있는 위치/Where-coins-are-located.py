8dc38561b50a132936853eef61fa80c52a371260
HA SEONG JIN <129818881+honeydanji@users.noreply.github.com>
2024-07-01 22:51:18 +0900

    Title: 동전이 있는 위치, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240701/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+arr = [[0 for _ in range(n)] for _ in range(n)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    arr[a-1][b-1] = 1
+
+for i in range(len(arr)):
+    print(' '.join(map(str, arr[i])))
