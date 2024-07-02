9f2bf13d246eda79d7015466d0e729465d27cbaa
HA SEONG JIN <129818881+honeydanji@users.noreply.github.com>
2024-07-02 13:54:07 +0900

    Title: Run Length 인코딩, Time: 147ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240702/Run Length 인코딩/run-length-encoding.py
+++ 240702/Run Length 인코딩/run-length-encoding.py
@@ -3,17 +3,22 @@ brr = []
 
 count = 1
 
-for i in range(1, len(arr)):
-    if arr[i-1] == arr[i]:
-        count += 1
-    else:
-        brr.append(arr[i-1])
-        brr.append(str(count))
-        count = 1
+if len(arr) > 0:
+    for i in range(1, len(arr)):
+        if arr[i-1] == arr[i]:
+            count += 1
+        else:
+            brr.append(arr[i-1])
+            brr.append(str(count))
+            count = 1
 
-    if i == len(arr)-1:
-        brr.append(arr[i])
-        brr.append(str(count))
+        if i == len(arr)-1:
+            brr.append(arr[i])
+            brr.append(str(count))
+
+else:
+    brr.append(arr[0])
+    brr.append(2)
 
 print(len(brr))
 print(''.join(map(str, brr)))
