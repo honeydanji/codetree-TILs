778edafcf1d16308a1badf1deac8b097112c91e1
HA SEONG JIN <129818881+honeydanji@users.noreply.github.com>
2024-07-02 13:34:54 +0900

    Title: Run Length 인코딩, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240702/Run Length 인코딩/run-length-encoding.py
@@ -0,0 +1,19 @@
+arr = input()
+brr = []
+
+
+count = 1
+
+for i in range(1, len(arr)):
+    if arr[i-1] == arr[i]:
+        count += 1
+    else:
+        brr.append(arr[i-1])
+        brr.append(str(count))
+        count = 1
+
+brr.append(arr[i-1])
+brr.append(str(count))
+
+print(len(brr))
+print(''.join(map(str, brr)))
