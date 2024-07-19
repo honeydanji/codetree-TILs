ea83e76bfd0a03afeab54109e09b8867944f6f4a
HA SEONG JIN <129818881+honeydanji@users.noreply.github.com>
2024-07-19 17:28:11 +0900

    Title: 미는 횟수, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/미는 횟수/number-of-pushes.py
+++ 240719/미는 횟수/number-of-pushes.py
@@ -1,16 +1,17 @@
 a = input()
 b = input()
 count = 0
+check = False
 
-
-for i in range(len(a)):
+for i in range(len(a)-1):
     count += 1
     a = a[-1] + a[:len(a)-1]
 
     if a == b:
+        check = True
         break
 
-if count == 0:
+if check == False:
     print(-1)
 else:
     print(count)
