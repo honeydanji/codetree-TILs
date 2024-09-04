b8cd35e6d104088c96ae0606482104dd7eae3fe0
HA SEONG JIN <129818881+honeydanji@users.noreply.github.com>
2024-09-04 21:57:40 +0900

    Title: 2021년 날짜의 유무, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240904/2021년 날짜의 유무/with-or-without-2021.py
+++ 240904/2021년 날짜의 유무/with-or-without-2021.py
@@ -2,12 +2,9 @@ def check(m, d):
     if m >= 1 and m <= 12:
         if m == '2' and d >= 1 and d <= 28:
             return True
-        else:
-            return False
-
-        if m % 2 == 0 and d >= 1 and d <= 30:
+        elif m % 2 == 0 and m != 8 and m != 2 and d >= 1 and d <= 30:
             return True
-        elif m % 2 != 0 and d >= 1 and d <= 31:
+        elif (m % 2 != 0 or m == 8) and d >= 1 and d <= 31:
             return True
     else:
         return False
