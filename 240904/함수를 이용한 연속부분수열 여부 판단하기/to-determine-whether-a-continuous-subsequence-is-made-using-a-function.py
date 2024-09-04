a7032626b1bf7ec9a388737ba1e408ea622544ff
HA SEONG JIN <129818881+honeydanji@users.noreply.github.com>
2024-09-04 21:38:21 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240904/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240904/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,5 +1,5 @@
 def sequence_check(a_li, b_li, a, b):
-    for i in range(0, a-b):
+    for i in range(0, a-b+1):
         if str(a_li[i:i+b]) == str(b_li):
             return True    
     return False
